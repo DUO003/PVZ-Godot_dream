75977cb010c5063c230b6b5a239be92210255977
D003 <2757884203@qq.com>
2025-07-04 09:59:48 +0800

    BUG修复
    阳光土豆雷，被铲掉不在掉落阳光，未长大也不在掉落阳光

--- FZB/ZW/zj_002_ygtdl.gd
+++ FZB/ZW/zj_002_ygtdl.gd
@@ -8,12 +8,18 @@ func _ready():
 	super._ready()
 	day_suns = get_tree().root.get_node("MainGame/DaySuns")
 
+#region 植物死亡相关
 func _plant_free():
-	var new_sun = sun_scene.instantiate()
-	if new_sun is Sun:
+	if is_armed:#判断阳光土豆雷是否长大
+		var new_sun = sun_scene.instantiate()
+		if new_sun is Sun:
 		
-		day_suns.add_child(new_sun)
-		new_sun.sun_value = 阳光产量
-		new_sun._sun_scale(阳光产量)
-		new_sun.global_position = sun.global_position
+			day_suns.add_child(new_sun)
+			new_sun.sun_value = 阳光产量
+			new_sun._sun_scale(阳光产量)
+			new_sun.global_position = sun.global_position
 	super._plant_free()
+# 铲掉植物
+func be_shovel_kill():
+	self.queue_free()
+#endregion
